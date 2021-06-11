50baa99cdbb0bba6fb7451237b98d9b88c9fc6b5
Zayne <llz981011@hotmail.com>
2021-06-11 13:52:02 +0100

    Update TextFile.swift
    switch to `Natural Language` library for multi-language support.

--- Sources/Custodian/Utilities/TextFile.swift
+++ Sources/Custodian/Utilities/TextFile.swift
@@ -6,26 +6,45 @@
 //
 
 import Foundation
+import NaturalLanguage
 
 extension File {}
 
 class TextFile: File {
+	@available(macOS 10.14, *)
 	override func index() {
-		#warning("index")
+		// Read `.txt` file content
+		let fileContent: String = try! String(
+			contentsOfFile: url.path,
+			encoding: String.Encoding.utf8
+		)
 
-		let fileContent = try! String(contentsOfFile: url.path, encoding: String.Encoding.utf8)
+		let wordTokenizer = NLTokenizer(unit: .word)
+		wordTokenizer.string = fileContent
+		// Enumerate every word in the file.
+		wordTokenizer.enumerateTokens(in: fileContent.startIndex ..< fileContent.endIndex) { wordTokenRange, _ in
+			let word = fileContent[wordTokenRange].lowercased()
 
-		// Extract all lines in text file
-		lines = fileContent.components(separatedBy: .newlines)
+			#warning("Highly experimental")
+			if word.count <= 2 { return true }
 
-		// Extract every word from each line
-		for line in lines {
-			let words = line.components(
-				separatedBy: .whitespaces.union(CharacterSet([","]))
-			)
-			words.forEach { word in
-				addToThumbnail(s: word)
+			addToThumbnail(s: word)
+
+			if !wordSet.contains(word) {
+				wordSet.insert(word)
+
+				// Conataining folder's keywords index
+				if containingFolder!.thumbnail.keys.contains(word) {
+					containingFolder!.thumbnail[word]!.append(self)
+				}
 			}
+
+			return true
 		}
+	}
+
+	override func search(keyword: String) -> [String: Any] {
+		keyword
+		return [:]
 	}
 }
