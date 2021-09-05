27bc53696f0cb6ab5a1538e0b0183bccb56ff679
Zayne Liu <zayneliu@hotmail.com>
2021-09-05 19:18:24 +0100

    remove all file indexed test
    as only supported file types will be indexed.

--- Tests/CustodianTests/ModelsTests/FolderTests.swift
+++ Tests/CustodianTests/ModelsTests/FolderTests.swift
@@ -14,16 +14,32 @@ final class FolderTests: XCTestCase {
 		isDirectory: true
 	)
 
-	func testIndexFileCount() {
+	// func testIndexFileCount() {
+	//	let folder = Folder(url: url)
+	//	try! folder.index()
+	//
+	//	// work out the number of files automatically
+	//	var expected = 0
+	//	for case let file as String in FileManager.default.enumerator(atPath: url.path)! {
+	//		let fileUrl = url.appendingPathComponent(file)
+	//		if !fileUrl.hasDirectoryPath,
+	//		   !fileUrl.lastPathComponent.starts(with: ["."]) { expected += 1 }
+	//	}
+	//
+	//	let actual = folder.files.count
+	//
+	//	XCTAssertEqual(expected, actual)
+	// }
+
+	func testIndexSupportedFileTypes() {
 		let folder = Folder(url: url)
 		try! folder.index()
-
-		// work out the number of files automatically
 		var expected = 0
+
 		for case let file as String in FileManager.default.enumerator(atPath: url.path)! {
 			let fileUrl = url.appendingPathComponent(file)
-			if !fileUrl.hasDirectoryPath,
-			   !fileUrl.lastPathComponent.starts(with: ["."]) { expected += 1 }
+			if FileFactory.allowedExt.contains(fileUrl.pathExtension.lowercased())
+			{ expected += 1 }
 		}
 
 		let actual = folder.files.count
