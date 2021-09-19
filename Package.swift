63be1e9495c9df6d952f92a5608d8332b61fe3ab
Zayne Liu <zayneliu@hotmail.com>
2021-09-19 19:09:28 +0100

    Update Package.swift
    fixed dep Ink

--- Package.swift
+++ Package.swift
@@ -15,8 +15,7 @@ let package = Package(
 	dependencies: [
 		// Dependencies declare other packages that this package depends on.
 		// .package(url: /* package url */, from: "1.0.0"),
-
-		.package(url: "https://github.com/johnsundell/ink.git", from: "0.5.1"),
+		.package(name: "Ink", url: "https://github.com/johnsundell/ink.git", from: "0.5.1"),
 	],
 	targets: [
 		// Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -24,7 +23,7 @@ let package = Package(
 		.target(
 			name: "Custodian",
 			dependencies: [
-				.product(name: "Ink", package: "ink"),
+				.product(name: "Ink", package: "Ink"),
 			]
 		),
 		.testTarget(
