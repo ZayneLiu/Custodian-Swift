ff62c4f78a7a0fcc654710804066efdb901a1e41
Zayne Liu <zayneliu@hotmail.com>
2021-11-27 15:22:37 +0000

    Update FileFactory.swift

--- Sources/Custodian/FileFactory.swift
+++ Sources/Custodian/FileFactory.swift
@@ -10,22 +10,23 @@ import Foundation
 @available(iOS 12.0, *)
 @available(macOS 10.15, *)
 class FileFactory {
-	private init() {}
+    private init() {
+    }
 
-	static let allowedExt = ["txt", "md"]
+    static let allowedExt = ["txt", "md"]
 
-	static func createFile(url: URL, folderUrl: URL) -> File {
-		let fileExt = url.pathExtension.lowercased()
-		if allowedExt.contains(fileExt) {
-			switch fileExt {
-				case "txt":
-					return TextFile(url: url, containingFolderUrl: folderUrl)
-				case "md":
-					return MardownFile(url: url, containingFolderUrl: folderUrl)
-				default:
-					break
-			}
-		}
-		return File(url: url, containingFolderUrl: folderUrl)
-	}
+    static func createFile(url: URL, folderUrl: URL) -> File? {
+        let fileExt = url.pathExtension.lowercased()
+        if allowedExt.contains(fileExt) {
+            switch fileExt {
+                case "txt":
+                    return TextFile(url: url, containingFolderUrl: folderUrl)
+                case "md":
+                    return MarkdownFile(url: url, containingFolderUrl: folderUrl)
+                default:
+                    break
+            }
+        }
+        return nil
+    }
 }
