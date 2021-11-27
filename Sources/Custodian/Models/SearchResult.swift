101995ad3e78441c278ca07b68f84cdb956d5179
Zayne Liu <zayneliu@hotmail.com>
2021-11-27 15:28:26 +0000

    Update SearchResult.swift

--- Sources/Custodian/Models/SearchResult.swift
+++ Sources/Custodian/Models/SearchResult.swift
@@ -10,12 +10,9 @@ import Foundation
 @available(macOS 10.15, *)
 public class SearchResult {
 	public let id = UUID()
-	public var file: File
-	public var keyword: String
+	public var file:        File
+	public var keyword:     String
 	public var occurrences: [Substring]
-	/// A dictionary of each word and its occurances in a specific file.
-
-	/// All lines in current file.
 
 	public init(file: File, keyword: String, occurrences: [Range<String.Index>]) {
 		self.file = file
@@ -23,8 +20,8 @@ public class SearchResult {
 		self.occurrences = occurrences.map { index in
 			file.fileContent[index]
 		}
-//		name = self.url.lastPathComponent
-//		type = FileType.Text
-//		ext = url.pathExtension
+		// name = self.url.lastPathComponent
+		// type = FileType.Text
+		// ext = url.pathExtension
 	}
 }
