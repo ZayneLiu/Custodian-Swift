b006c65dfdb40a55cb30da3f812cd8cb274be92f
Zayne Liu <zayneliu@hotmail.com>
2021-12-15 06:40:50 +0000

    Update FileFactory.swift

--- Sources/Custodian/FileFactory.swift
+++ Sources/Custodian/FileFactory.swift
@@ -12,8 +12,12 @@ import Foundation
 public class FileFactory {
 	private init() {}
 
-	static let allowedExt = ["txt", "md", "html", "docx", "pptx", "pdf", "rtf"]
+	//    static let msDocExt =
+	static let allowedExt = [
+		"txt", "md", "html", "docx", "docm", "pptx", "pptm", "pdf", "rtf", "odt", "odp",
+	]
 
+	//    "xlsx"
 	static func indexFile(url: URL, folderUrl: URL) -> File? {
 		let fileExt = url.pathExtension.lowercased()
 		if allowedExt.contains(fileExt) {
@@ -24,14 +28,20 @@ public class FileFactory {
 					return Markdown(url: url, containingFolderUrl: folderUrl)
 				case "html":
 					return HTML(url: url, containingFolderUrl: folderUrl)
-				case "docx":
-					return Word(url: url, containingFolderUrl: folderUrl)
-				case "pptx":
-					return PowerPoint(url: url, containingFolderUrl: folderUrl)
+				case "docx", "docm":
+					return MSWord(url: url, containingFolderUrl: folderUrl)
+				case "pptx", "pptm":
+					return MSPowerPoint(url: url, containingFolderUrl: folderUrl)
 				case "pdf":
 					return PDF(url: url, containingFolderUrl: folderUrl)
-            case "rtf":
-                return RichText(url: url, containingFolderUrl: folderUrl)
+				case "rtf":
+					return RichText(url: url, containingFolderUrl: folderUrl)
+				case "odt":
+					return ODText(url: url, containingFolderUrl: folderUrl)
+				case "odp":
+					return ODPresentation(url: url, containingFolderUrl: folderUrl)
+					// case "xlsx":
+					//	 return MSExcel(url: url, containingFolderUrl: folderUrl)
 				default:
 					break
 			}
